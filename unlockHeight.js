2dd3d4e5c4805b7d7c0820541d2b90342dc849dc
a1300 <matthias-knopp@gmx.net>
2018-08-12 21:16:24 +0200

    add unlock height

---
+++ unlockHeight.js
@@ -0,0 +1,30 @@
+
+let url = "http://localhost:4096/peer/transactions"
+let aschJS = require('asch-js')
+
+const axios = require('axios')
+const secret = "sentence weasel match weather apple onion release keen lens deal fruit matrix"
+
+
+let trs = aschJS.transaction.unlock(secret)
+
+let data = {
+  transaction: trs
+}
+
+let headers = {
+  headers: {
+    magic: "594fe0f3",
+    version: ''
+  }
+}
+
+axios.post(url, data, headers)
+  .then((response) => {
+    console.log(`${JSON.stringify(response.data, null, 2)}`)
+  })
+  .catch(erro => {
+    console.log(JSON.stringify(erro.message))
+  })
+
+
